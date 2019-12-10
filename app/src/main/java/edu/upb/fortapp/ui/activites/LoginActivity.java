bc4b5e05c607860329819d4175981cc907c811bf
SergioPerez32 <sergiorlando10@hotmail.com>
2019-12-10 00:36:12 -0400

    ahora si guerra civil

--- app/src/main/java/edu/upb/fortapp/ui/activites/LoginActivity.java
+++ app/src/main/java/edu/upb/fortapp/ui/activites/LoginActivity.java
@@ -1,4 +1,36 @@
 package edu.upb.fortapp.ui.activites;
 
-public class LoginActivity {
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.upb.fortapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+        private Button login;
+        private Context context;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            this.context=this;
+            setContentView(R.layout.login_layout_prueba);
+            this.login=findViewById(R.id.button4);
+
+            this.login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, MenuActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+        }
+    public void login(View view) {
+
+    }
 }
