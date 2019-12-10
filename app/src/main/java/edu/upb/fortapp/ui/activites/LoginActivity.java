06b2a522dd6c0491f4a6b171fac49945c6fd54f1
SergioPerez32 <sergiorlando10@hotmail.com>
2019-12-10 19:55:08 -0400

    Navegacion 3.0

--- app/src/main/java/edu/upb/fortapp/ui/activites/LoginActivity.java
+++ app/src/main/java/edu/upb/fortapp/ui/activites/LoginActivity.java
@@ -30,7 +30,4 @@ public class LoginActivity extends AppCompatActivity {
             });
 
         }
-    public void login(View view) {
-
-    }
 }
--- app/src/main/java/edu/upb/fortapp/ui/activites/MenuActivity.java
+++ app/src/main/java/edu/upb/fortapp/ui/activites/MenuActivity.java
@@ -1,15 +1,33 @@
 package edu.upb.fortapp.ui.activites;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.upb.fortapp.R;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button map;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.context=this;
         setContentView(R.layout.activity_menu);
+        this.map=findViewById(R.id.button11);
+
+        this.map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
