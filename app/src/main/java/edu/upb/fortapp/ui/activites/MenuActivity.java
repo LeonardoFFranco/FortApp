80b67997f6143e426d3a0e60d450f318eca02bda
SergioPerez32 <sergiorlando10@hotmail.com>
2019-12-10 09:03:51 -0400

    Correccion navigation

--- app/src/main/java/edu/upb/fortapp/ui/activites/MenuActivity.java
+++ app/src/main/java/edu/upb/fortapp/ui/activites/MenuActivity.java
@@ -1,5 +1,15 @@
 package edu.upb.fortapp.ui.activites;
 
-public class MenuActivity {
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.upb.fortapp.R;
+
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
 }
