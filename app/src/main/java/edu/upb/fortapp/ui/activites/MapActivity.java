95f2214ca13fe7463b120804e036bf63ffe1f00a
luchio10 <luisegp10@hotmail.com>
2019-12-09 17:21:34 -0800

    Mapa layout, nombres de imagenes

--- app/src/main/java/edu/upb/fortapp/ui/activites/MapActivity.java
+++ app/src/main/java/edu/upb/fortapp/ui/activites/MapActivity.java
@@ -1,4 +1,17 @@
 package edu.upb.fortapp.ui.activites;
 
-public class MapActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.upb.fortapp.R;
+
+public class MapActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+
+
 }
