2b62213df55ab6f2a0d66ea0d43727221c0b9bbb
luchio10 <luisegp10@hotmail.com>
2019-12-09 18:51:25 -0800

    Mapa Zoom

--- app/src/main/java/edu/upb/fortapp/ui/activites/MapActivity.java
+++ app/src/main/java/edu/upb/fortapp/ui/activites/MapActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.chrisbanes.photoview.PhotoView;
+
 import edu.upb.fortapp.R;
 
 public class MapActivity extends AppCompatActivity {
@@ -11,7 +13,13 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        //photoView Zoom
+        PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
+        photoView.setImageResource(R.drawable.fornite2); //nombre de la imagen
+        //photoView Zoom
     }
+
+
 
 
 
