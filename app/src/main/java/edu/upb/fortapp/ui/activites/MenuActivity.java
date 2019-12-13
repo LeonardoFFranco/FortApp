e3522d3676e9fafc52190fc4a081172b89867874
SergioPerez32 <sergiorlando10@hotmail.com>
2019-12-12 22:19:50 -0400

    Fragment Dialog

--- app/src/main/java/edu/upb/fortapp/ui/activites/MenuActivity.java
+++ app/src/main/java/edu/upb/fortapp/ui/activites/MenuActivity.java
@@ -1,6 +1,5 @@
 package edu.upb.fortapp.ui.activites;
 
-import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,9 +7,11 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import edu.upb.fortapp.R;
 import edu.upb.fortapp.ui.fragments.MyDialogFragment;
+
 public class MenuActivity extends AppCompatActivity {
 
     private Button map;
@@ -20,7 +21,6 @@ public class MenuActivity extends AppCompatActivity {
     private Button itemshop;
     private Button information;
     private Context context;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,9 +73,9 @@ public class MenuActivity extends AppCompatActivity {
         this.information.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentManager fm = getFragmentManager();
+                FragmentManager fm = getSupportFragmentManager();
                 MyDialogFragment dialogFragment = new MyDialogFragment ();
-               // dialogFragment.show(fm, "Sample Fragment");
+                dialogFragment.show( fm, "Sample Fragment");
             }
         });
 
--- app/src/main/java/edu/upb/fortapp/ui/fragments/MyDialogFragment.java
+++ app/src/main/java/edu/upb/fortapp/ui/fragments/MyDialogFragment.java
@@ -14,7 +14,7 @@ public class MyDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_sample_dialog, container, false);
-        getDialog().setTitle("Simple Dialog");
+        getDialog().setTitle("Information");
         Button dismiss = (Button) rootView.findViewById(R.id.dismiss);
         dismiss.setOnClickListener(new View.OnClickListener() {
             @Override
