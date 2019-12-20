c6a28a574d4a9e9993cd77f87a040cf89992a1c7
SergioPerez32 <sergiorlando10@hotmail.com>
2019-12-20 02:17:36 -0400

    Complemento Datos Mock

--- app/src/main/java/edu/upb/fortapp/ui/activites/utils/bpElemUtils.java
+++ app/src/main/java/edu/upb/fortapp/ui/activites/utils/bpElemUtils.java
@@ -9,16 +9,16 @@ public class bpElemUtils {
 
     public static List<bpElemMock> getBattlePassElements() {
         List<bpElemMock> battlePassElements = new ArrayList<>();
-        battlePassElements.add(new bpElemMock(1, 1,R.drawable.bp1, "Nombre Premio1"));
-        battlePassElements.add(new bpElemMock(2, 2,R.drawable.bp1, "Nombre Premio2"));
-        battlePassElements.add(new bpElemMock(3, 3,R.drawable.bp1, "Nombre Premio3"));
-        battlePassElements.add(new bpElemMock(4, 4,R.drawable.bp1, "Nombre Premio4"));
-        battlePassElements.add(new bpElemMock(5, 5,R.drawable.bp1, "Nombre Premio5"));
-        battlePassElements.add(new bpElemMock(6, 6,R.drawable.bp1, "Nombre Premio6"));
-        battlePassElements.add(new bpElemMock(7, 7,R.drawable.bp1, "Nombre Premio7"));
-        battlePassElements.add(new bpElemMock(8, 8,R.drawable.bp1, "Nombre Premio8"));
-        battlePassElements.add(new bpElemMock(9, 9,R.drawable.bp1, "Nombre Premio9"));
-        battlePassElements.add(new bpElemMock(10, 10,R.drawable.bp1, "Nombre Premio10"));
+        battlePassElements.add(new bpElemMock(1, 1,R.drawable.bp1, "1000 V-Bucks"));
+        battlePassElements.add(new bpElemMock(2, 2,R.drawable.bp2, "Raptor"));
+        battlePassElements.add(new bpElemMock(3, 3,R.drawable.bp1, "1000 V-Bucks"));
+        battlePassElements.add(new bpElemMock(4, 4,R.drawable.bp3, "John Wick"));
+        battlePassElements.add(new bpElemMock(5, 5,R.drawable.bp1, "1000 V-Bucks"));
+        battlePassElements.add(new bpElemMock(6, 6,R.drawable.bp4, "Royale Bomber"));
+        battlePassElements.add(new bpElemMock(7, 7,R.drawable.bp1, "1000 V-Bucks"));
+        battlePassElements.add(new bpElemMock(8, 8,R.drawable.bp5, "Flare"));
+        battlePassElements.add(new bpElemMock(9, 9,R.drawable.bp1, "1000 V-Bucks"));
+        battlePassElements.add(new bpElemMock(10, 10,R.drawable.bp6, "Absolute Zero"));
         return battlePassElements;
     }
 }
